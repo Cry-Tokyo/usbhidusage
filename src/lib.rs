afada66c1e80b6bfd0374e035541e6880bc904fe
Cry-Tokyo <114428720+Cry-Tokyo@users.noreply.github.com>
2024-10-27 11:22:05 -0400

    Update and rename main.rs to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+//!
+//!
+//!
+//!
+pub mod arcade;
+pub mod auxiliary_display;
+pub mod barcode_scanner;
+pub mod battery_system;
+pub mod braille_display;
+pub mod button;
+pub mod camera_control;
+pub mod consumer;
+pub mod digitizers;
+pub mod eye_and_head_trackers;
+pub mod fast_identify_online_alliance;
+pub mod game_controls;
+pub mod gaming_device;
+pub mod generic_desktop;
+pub mod generic_device_controls;
+pub mod haptics;
+pub mod keyboard;
+pub mod led;
+pub mod lighting_and_illumination;
+pub mod magnetic_stripereader;
+pub mod medical_instrument;
+pub mod monitor;
+pub mod monitor_enumerated;
+pub mod ordinal;
+pub mod physical_input_device;
+pub mod power;
+pub mod preferred_colors;
+pub mod scales;
+pub mod sensors;
+pub mod simulation_controls;
+pub mod sport_controls;
+pub mod system_on_chip;
+pub mod telephony_device;
+pub mod unicode;
+pub mod vesa_virtual_controls;
+pub mod vr_controls;
